4d0d23c3f9c43149fd1853ca0ebc1a994028bd3c
Nikita <nikita2003.ns@gmail.com>
2021-11-19 16:29:06 +0300

    FIX: delete unnecessary print()

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ print("Enter name of the sheet: ")
 name_of_sheet=input()
 print("Enter name of the colon or colons:  ")
 name_of_colon=input().split()
-print (name_of_colon)
 with open('Excel.txt', 'w') as file:
     pd.concat(pd.read_excel(way_to_file,
                             sheet_name=[name_of_sheet],
                             usecols=name_of_colon)).to_string(file, index=False)
-
 print("File 'Excel.txt' is ready")
 print("-------------------------")
 input()
