5852c451b8d95d29290dd1e8c96211b2ce98b140
Nikita <nikita2003.ns@gmail.com>
2021-11-19 15:57:22 +0300

    Release v1.0

---
+++ main.py
@@ -0,0 +1,17 @@
+#Парсер табличного текста из Excel документа в текстовый файл.
+import pandas as pd
+print("Hello! Paste the way to your Excel file here:")
+way_to_file=input()
+print("Enter name of the sheet: ")
+name_of_sheet=input()
+print("Enter name of the colon or colons:  ")
+name_of_colon=input().split()
+print (name_of_colon)
+with open('Excel.txt', 'w') as file:
+    pd.concat(pd.read_excel(way_to_file,
+                            sheet_name=[name_of_sheet],
+                            usecols=name_of_colon)).to_string(file, index=False)
+
+print("File 'Excel.txt' is ready")
+print("-------------------------")
+input()
